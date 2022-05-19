7d9655e4fa441b460ac67d42678d7807cb1b1ff0
Kimdaehwan <etukdh6433@naver.com>
2022-05-19 21:16:35 +0900

    2022-05-19 Solve problem 10837

--- Kimdaehwan_StudyGroup/12week_etukdh6433.py
+++ Kimdaehwan_StudyGroup/12week_etukdh6433.py
@@ -32,25 +32,26 @@
 #
 # k = int(sys.stdin.readline())
 # c = int(sys.stdin.readline())
-# lc = []
 #
 # for i in range(c):
-#     lc.append(list(map(int, sys.stdin.readline().split())))
-#     m, n = 0, 0
-#     for j in range(k):
-#         r = k - j - 1
-#         if m != lc[i][0]:
-#             m += 1
-#         if m + r < n:
-#             break
-#         if n != lc[i][1]:
-#             n += 1
-#         if n + r < m:
-#             break
-#     if lc[i] == [m, n]:
+#     m, n = map(int, sys.stdin.readline().split())
+#     # 두 점수의 차이
+#     gap = abs(m - n)
+#     # 누구도 점수를 얻지 못하는 라운드
+#     r = k - max(m, n)
+#
+#     if m == n:
 #         print(1)
-#     else:
-#         print(0)
+#     elif m > n:
+#         if gap - r <= 2:
+#             print(1)
+#         else:
+#             print(0)
+#     elif m < n:
+#         if gap - r <= 1:
+#             print(1)
+#         else:
+#             print(0)
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
