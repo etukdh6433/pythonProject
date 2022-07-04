2a98f56804c2b4496c6c14b3ee9b2a1dd1f785e0
Kimdaehwan <etukdh6433@naver.com>
2022-07-04 20:48:26 +0900

    2022-07-04 Solve 2206

--- Kimdaehwan_StudyGroup/17week_etukdh6433.py
+++ Kimdaehwan_StudyGroup/17week_etukdh6433.py
@@ -1,53 +1,46 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+17주차 2206 벽 부수고 이동하기
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# import sys
+# from collections import deque
+#
+#
+# def find(arr):
+#     dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+#     dist = [[[0, 0] for i in range(c)] for j in range(r)]
+#     dist[0][0][0] = 1
+#     q = deque([(0, 0, 0)])
+#
+#     while q:
+#         pr, pc, w = q.popleft()
+#
+#         if (pr == r - 1) and (pc == c - 1):
+#             return dist[-1][-1][w]
+#
+#         for k in range(4):
+#             nr = pr + dir[k][0]
+#             nc = pc + dir[k][1]
+#
+#             if (0 <= nr) and (nr < r) and (0 <= nc) and (nc < c) and (dist[nr][nc][w] == 0):
+#
+#                 if arr[nr][nc] == 0:
+#                     q.append((nr, nc, w))
+#                     dist[nr][nc][w] = dist[pr][pc][w] + 1
+#
+#                 if w == 0 and arr[nr][nc] == 1:
+#                     q.append((nr, nc, 1))
+#                     dist[nr][nc][1] = dist[pr][pc][w] + 1
+#     return -1
+#
+#
+# r, c = map(int, sys.stdin.readline().split())
+# nl = []
+# for i in range(r):
+#     nl.append(list(map(int, sys.stdin.readline().rstrip())))
+#
+# print(find(nl))
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 17주차 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-17주차 2206 벽 부수고 이동하기 (시간초과)
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-import sys
-import math
-import time
-
-def find(arr):
-    dr = [-1, 0, 1, 0]
-    dc = [0, 1, 0, -1]
-    dist = [[math.inf for i in range(c)] for j in range(r)]
-    dist[0][0] = 1
-    cnt = 0
-    o = 0
-    q = []
-    q.append((0, 0))
-    while q:
-        t = q.pop(0)
-        if dist[t[0]][t[1]] == math.inf:
-            continue
-        for k in range(4):
-            nr = t[0] + dr[k]
-            nc = t[1] + dc[k]
-            if (nr >= 0) and (nr < r) and (nc >= 0) and (nc < c) and (dist[nr][nc] > dist[t[0]][t[1]] + 1):
-                if arr[nr][nc] == 0:
-                    dist[nr][nc] = dist[t[0]][t[1]] + 1
-                    q.append((nr, nc))
-                elif cnt < 1:
-                    dist[nr][nc] = dist[t[0]][t[1]] + 1
-                    q.append((nr, nc))
-                    o = 1
-                elif cnt >= 1:
-                    q.append((nr, nc))
-        if o == 1:
-            cnt += 1
-
-    return dist
-
-
-r, c = map(int, sys.stdin.readline().split())
-nl = []
-for i in range(r):
-    nl.append(list(map(int, sys.stdin.readline().rstrip())))
-
-dist = find(nl)
-
-if dist[r-1][c-1] == math.inf:
-    print(-1)
-else:
-    print(f"{dist[r-1][c-1]}")
