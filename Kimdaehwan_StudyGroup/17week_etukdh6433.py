7573e8ba6f1e10e1fd33d9911d2b95ae79365507
Kimdaehwan <etukdh6433@naver.com>
2022-07-07 10:48:35 +0900

    2022-07-05 Solve 3055

--- Kimdaehwan_StudyGroup/17week_etukdh6433.py
+++ Kimdaehwan_StudyGroup/17week_etukdh6433.py
@@ -7,16 +7,20 @@
 #
 # def find(arr):
 #     dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+#     # 해당 위치까지의 거리, 0 : 벽을 부수지 않음, 1 : 벽을 부숨
 #     dist = [[[0, 0] for i in range(c)] for j in range(r)]
 #     dist[0][0][0] = 1
+#     # 시작 위치를 큐에 입력
 #     q = deque([(0, 0, 0)])
 #
 #     while q:
+#         # 현재 위치와 벽의 파괴여부를 입력
 #         pr, pc, w = q.popleft()
 #
 #         if (pr == r - 1) and (pc == c - 1):
 #             return dist[-1][-1][w]
 #
+#         # 해당 위치의 사방을 확인
 #         for k in range(4):
 #             nr = pr + dir[k][0]
 #             nc = pc + dir[k][1]
@@ -57,6 +61,7 @@
 #
 #     for i in range(r):
 #         for j in range(c):
+#             # 각 조건에서 True로 변경하여 해당 위치로 가지 못 하도록 변경
 #             if arr[i][j] == '*':
 #                 water.append((i, j))
 #                 dist[i][j] = True
@@ -68,7 +73,9 @@
 #
 #     while sonic:
 #         for i in range(len(water)):
+#             # 물의 시작 위치를 pop하여 입력
 #             wx, wy = water.popleft()
+#             # 시작 위치의 사방을 탐색
 #             for j in range(4):
 #                 nx = wx + dir[j][0]
 #                 ny = wy + dir[j][1]
