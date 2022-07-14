a51fe51ee8755376624452ba23ff11d11a2637d1
Kimdaehwan <etukdh6433@naver.com>
2022-07-14 14:48:46 +0900

    2022-07-14 Solve 24479

--- Kimdaehwan_StudyGroup/18week_etukdh6433.py
+++ Kimdaehwan_StudyGroup/18week_etukdh6433.py
@@ -48,5 +48,42 @@
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-18주차 14503 로봇 청소기
+18주차 24479 알고리즘 수업 - 깊이 우선 탐색
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# import sys
+# sys.setrecursionlimit(10**6)
+#
+#
+# def dfs(V, E, r, order):
+#     V[r] = order
+#     e = E[r]
+#
+#     for j in e:
+#         if V[j] == 0:
+#             order = dfs(V, E, j, order + 1)
+#
+#     return order
+#
+#
+# n, m, r = map(int, sys.stdin.readline().split())
+# V = [0 for _ in range(n + 1)]
+# E = [[] for _ in range(n + 1)]
+#
+# for _ in range(m):
+#     a, b = map(int, sys.stdin.readline().split())
+#     E[a].append(b)
+#     E[b].append(a)
+#
+# for i in range(1, n + 1):
+#     E[i].sort()
+#
+# order = 1
+# dfs(V, E, r, order)
+#
+# for i in range(1, n + 1):
+#     print(V[i])
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+18주차 24479 알고리즘 수업 - 넓이 우선 탐색
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
