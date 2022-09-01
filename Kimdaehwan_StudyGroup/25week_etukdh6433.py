110ce5e6872226725c71a1d557582e82481e8ffe
Kimdaehwan <etukdh6433@naver.com>
2022-09-01 12:59:21 +0900

    22-08-30 Perfectly Solve 2156

--- Kimdaehwan_StudyGroup/25week_etukdh6433.py
+++ Kimdaehwan_StudyGroup/25week_etukdh6433.py
@@ -1,5 +1,5 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-25주차 2156 포도주 주식 (오답)
+25주차 2156 포도주 주식
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 # import sys
 #
@@ -19,11 +19,17 @@
 #         result[1] = wine[1]
 #         result[2] = wine[2] + wine[1]
 #
-#     elif n >= 3:
+#     elif n == 3:
 #         result[1] = wine[1]
 #         result[2] = wine[2] + wine[1]
-#         for i in range(3, N + 1):
-#             result[i] = wine[i] + max(result[i - 2], wine[i - 1] + result[i - 3])
+#         result[3] = wine[3] + max(result[1], wine[2] + result[0])
+#
+#     elif n >= 4:
+#         result[1] = wine[1]
+#         result[2] = wine[2] + wine[1]
+#         result[3] = wine[3] + max(result[1], wine[2] + result[0])
+#         for i in range(4, N + 1):
+#             result[i] = wine[i] + max(result[i - 2], wine[i - 1] + result[i - 3], wine[i - 1] + result[i - 4])
 #
 #     return result
 #
@@ -31,3 +37,15 @@
 # result = sum_wine(N)
 #
 # print(max(result))
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+25주차 15686 치킨 배달
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# import sys
+#
+# N, M = map(int, sys.stdin.readline().split())
+# field = []
+#
+# for _ in range(N):
+#     field.append(list(map(int, sys.stdin.readline().split())))
